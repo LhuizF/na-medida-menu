1f4fcb882e4e013a3d14fcd63b5ed910721a95ec
Luiz Henrique Ferreira <luizhff@gmail.com>
2023-09-25 20:35:54 -0300

    feat: add useCart

--- src/components/ListOptions.tsx
+++ src/components/ListOptions.tsx
@@ -1,44 +1,66 @@
+"use client";
+
+import { ButtonGroup, IconButton } from "@mui/material";
+import { Remove, Add, DeleteForever } from "@mui/icons-material";
+import { useCart } from "@/contexts/cart";
+
 interface Props {
   options: IOptionDB[];
 }
-import { ButtonGroup, Button, IconButton } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 export const ListOptions: React.FC<Props> = ({ options }) => {
+  const { addItem, removeItem, getItemAmount } = useCart();
+
   return (
     <div className="flex flex-col gap-4">
-      {options.map((option) => (
-        <div
-          key={option.id}
-          className="bg-white p-2 rounded-md font-semibold drop-shadow-md"
-        >
-          <div>
+      {options.map((option) => {
+        const totalItem = getItemAmount(option.id);
+
+        return (
+          <div
+            key={option.id}
+            className="bg-white p-2 rounded-md font-semibold drop-shadow-md"
+          >
             <div>
-              <p className="text-black">{option.name}</p>
-              <div className="flex">
-                <p className="text-xs text-gray-600">
-                  {option.code} {option.weight}
-                </p>
+              <div>
+                <p className="text-black">{option.name}</p>
+                <div className="flex">
+                  <p className="text-xs text-gray-600">
+                    {option.code} {option.weight}
+                  </p>
+                </div>
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="text-green-400">R$ {option.price}</p>
+                <ButtonGroup size="small" variant="contained">
+                  {totalItem > 0 && (
+                    <>
+                      <IconButton
+                        size="small"
+                        onClick={() => removeItem(option.id)}
+                      >
+                        {totalItem === 1 ? (
+                          <DeleteForever color="primary" />
+                        ) : (
+                          <Remove color="primary" />
+                        )}
+                      </IconButton>
+                      <div className="p-[5px]">
+                        <div className="text-black text-base w-6 h-6 text-center">
+                          {totalItem}
+                        </div>
+                      </div>
+                    </>
+                  )}
+                  <IconButton size="small" onClick={() => addItem(option)}>
+                    <Add color="primary" />
+                  </IconButton>
+                </ButtonGroup>
               </div>
             </div>
-            <div className="flex justify-between items-center">
-              <p className="text-green-400">R$ {option.price}</p>
-              <ButtonGroup size="small" variant="contained">
-                <IconButton size="small">
-                  <DeleteForeverIcon color="primary" />
-                </IconButton>
-                <IconButton disabled>
-                  <div className="text-black min-w-[18px] text-base">1</div>
-                </IconButton>
-                <IconButton size="small">
-                  <AddIcon color="primary" />
-                </IconButton>
-              </ButtonGroup>
-            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,10 +1,17 @@
+"use client";
+import { useCart } from "@/contexts/cart";
+
 export const TabBar: React.FC = () => {
+  const { getTotalItems, getTotalPrice } = useCart();
+
   return (
     <div className="bg-slate-600 sticky bottom-0 w-full h-12 px-4 flex items-center justify-end drop-shadow-[0_-3px_3px_rgba(0,0,0,0.50)]">
       <div className="flex gap-1  items-end">
-        <p className="text-base font-semibold">Total: R$13,50</p>
+        <p className="text-base font-semibold">Total: R${getTotalPrice()}</p>
         <div>
-          <span className="text-xs">1/item</span>
+          <span className="text-xs">
+            {getTotalItems()}/{getTotalItems() === 1 ? "item" : "items"}
+          </span>
         </div>
       </div>
     </div>
